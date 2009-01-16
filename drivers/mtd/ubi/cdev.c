d4c7d70da0e567100fa28c952c6f88337ffa0ffd
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2009-01-16 18:03:22 +0200

    UBI: allow all ioctls
    Some ioctl's in UBI are enabled only when debugging is switched
    on. There is not particular reason for this, just noone needed
    them. However, some people need the now for their user-space
    development. Thus, allow these ioctl's even if UBI debugging
    is disabled.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/cdev.c
+++ drivers/mtd/ubi/cdev.c
@@ -490,7 +490,6 @@ static int vol_cdev_ioctl(struct inode *inode, struct file *file,
 		break;
 	}
 
-#ifdef CONFIG_MTD_UBI_DEBUG_USERSPACE_IO
 	/* Logical eraseblock erasure command */
 	case UBI_IOCEBER:
 	{
@@ -563,7 +562,6 @@ static int vol_cdev_ioctl(struct inode *inode, struct file *file,
 		err = ubi_is_mapped(desc, lnum);
 		break;
 	}
-#endif
 
 	default:
 		err = -ENOTTY;
