32aec5fc65b3f52e293f761a295c4486a50a6e13
Martin Fuzzey <mfuzzey@gmail.com>
2009-06-01 09:19:37 +0100

    [ARM] 5534/1: kmalloc must return a cache line aligned buffer
    Define ARCH_KMALLOC_MINALIGN in asm/cache.h
    At the request of Russell also move ARCH_SLAB_MINALIGN to this file.
    
    Signed-off-by: Martin Fuzzey <mfuzzey@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/page.h
+++ arch/arm/include/asm/page.h
@@ -187,13 +187,6 @@ typedef struct page *pgtable_t;
 #define VM_DATA_DEFAULT_FLAGS	(VM_READ | VM_WRITE | VM_EXEC | \
 				 VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC)
 
-/*
- * With EABI on ARMv5 and above we must have 64-bit aligned slab pointers.
- */
-#if defined(CONFIG_AEABI) && (__LINUX_ARM_ARCH__ >= 5)
-#define ARCH_SLAB_MINALIGN 8
-#endif
-
 #include <asm-generic/page.h>
 
 #endif
