bd452efc0ad93316609e5cf14c41da327fb13829
Avinash.H.M <avinashhm@ti.com>
2011-05-20 21:16:24 +0530

    OMAP2/3: hwmod: fix the i2c-reset timeout during bootup
    The sequence of _ocp_softreset doesn't work for i2c. The i2c module has a
    special sequence to reset the module. The sequence is
     - Disable the I2C.
     - Write to SOFTRESET bit.
     - Enable the I2C.
     - Poll on the RESETDONE bit.
    The sequence is implemented as a function and the i2c_class is updated with
    the correct 'reset' pointer.  omap_hwmod_softreset function is implemented
    which triggers the softreset by writing into sysconfig register. On following
    this sequence, i2c module resets properly and timeouts are not seen.
    
    Cc: Rajendra Nayak <rnayak@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Kevin Hilman <khilman@ti.com>
    Signed-off-by: Avinash.H.M <avinashhm@ti.com>

--- arch/arm/plat-omap/include/plat/i2c.h
+++ arch/arm/plat-omap/include/plat/i2c.h
@@ -23,6 +23,8 @@
 
 #include <linux/i2c.h>
 
+struct omap_hwmod;
+
 #if defined(CONFIG_I2C_OMAP) || defined(CONFIG_I2C_OMAP_MODULE)
 extern int omap_register_i2c_bus(int bus_id, u32 clkrate,
 				 struct i2c_board_info const *info,
@@ -51,5 +53,7 @@ struct omap_i2c_dev_attr {
 
 void __init omap1_i2c_mux_pins(int bus_id);
 void __init omap2_i2c_mux_pins(int bus_id);
+
+int omap_i2c_reset(struct omap_hwmod *oh);
 
 #endif /* __ASM__ARCH_OMAP_I2C_H */
