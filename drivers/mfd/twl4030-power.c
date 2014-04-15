1fde514c59f9256f2cb5396823245c3da7909adc
Grazvydas Ignotas <notasas@gmail.com>
2014-04-16 02:36:24 +0300

    mfd: twl4030-power: warn anout P3 order, not P12

--- drivers/mfd/twl4030-power.c
+++ drivers/mfd/twl4030-power.c
@@ -441,12 +441,12 @@ static int __init load_twl4030_script(struct twl4030_script *tscript,
 		err = twl4030_config_wakeup12_sequence(address);
 		if (err)
 			goto out;
-		order = 1;
 	}
 	if (tscript->flags & TWL4030_WAKEUP3_SCRIPT) {
 		err = twl4030_config_wakeup3_sequence(address);
 		if (err)
 			goto out;
+		order = 1;
 	}
 	if (tscript->flags & TWL4030_SLEEP_SCRIPT) {
 		if (!order)
